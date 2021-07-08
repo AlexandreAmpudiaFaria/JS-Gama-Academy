eaaabec83facb9c9c1233242ea9873017d22f784
Alexandre Faria <alexandre_faria123@icloud.com>
2021-07-08 14:40:00 -0300

    Atualização aula 2 Gama Academy

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 
-var myName = "Alexandre";
-console.log(myName);
+var myName = "Alexandre"; // string
+
+var age = 23 // number
+
+var height = 1.76 // number
+
+null
+undefined
+
+vai isStundet = false // boolean
+
+// Objeto, array e function
+
+var person = {
+    "name": "Alexandre",
+    "age": 23,
+    "height": 1.76
+}
+
+var students = ["Simara", "Alexandre", "João"]
+
+function sum (a,b){
+    return a + b
+}
+
+//Operador typeof ---> mostra qual o tipo do parametro
+console.log(typeof age);
 
