4f0361aa891fc20ded04c8fdccfb50f92f8cb622
Alexandre Faria <alexandre_faria123@icloud.com>
2021-07-09 22:26:06 -0300

    aula 3 javascript Gama Academy

--- script.js
+++ script.js
@@ -25,5 +25,37 @@ function sum (a,b){
 }
 
 //Operador typeof ---> mostra qual o tipo do parametro
-console.log(typeof age);
+
+//soma
+var sumTwoNumbers = 1+3
+console.log(sumTwoNumbers);
+
+//subtração
+var subTwoNumbers = 4+3
+console.log(subTwoNumbers);
+
+//multiplicação
+var multTwoNumbers = 4*3
+console.log(multTwoNumbers);
+
+//divisão
+var divTwoNumbers = 4/2
+console.log(divTwoNumbers);
+
+//resto da divisão
+console.log(8%3);
+
+//incremento ++
+var num = 1;
+console.log(++num);
+num = num + 1.
+
+//descremento --
+var num = 2;
+console.log(--num);
+num = num - 1;
+
+
+
+
 
