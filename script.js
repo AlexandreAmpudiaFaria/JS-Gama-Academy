fdf747289f492087161f15b431fd546cbb403056
Alexandre Faria <alexandre_faria123@icloud.com>
2021-07-12 23:14:27 -0300

    Algoritmo FizzBuzz

--- script.js
+++ script.js
@@ -1,61 +1,18 @@
+let resultado = fizzBuzz(7);
+console.log(resultado);
 
-var myName = "Alexandre"; // string
-
-var age = 23 // number
-
-var height = 1.76 // number
-
-null
-undefined
-
-vai isStundet = false // boolean
-
-// Objeto, array e function
-
-var person = {
-    "name": "Alexandre",
-    "age": 23,
-    "height": 1.76
-}
-
-var students = ["Simara", "Alexandre", "João"]
-
-function sum (a,b){
-    return a + b
-}
-
-//Operador typeof ---> mostra qual o tipo do parametro
-
-//soma
-var sumTwoNumbers = 1+3
-console.log(sumTwoNumbers);
-
-//subtração
-var subTwoNumbers = 4+3
-console.log(subTwoNumbers);
-
-//multiplicação
-var multTwoNumbers = 4*3
-console.log(multTwoNumbers);
-
-//divisão
-var divTwoNumbers = 4/2
-console.log(divTwoNumbers);
-
-//resto da divisão
-console.log(8%3);
-
-//incremento ++
-var num = 1;
-console.log(++num);
-num = num + 1.
-
-//descremento --
-var num = 2;
-console.log(--num);
-num = num - 1;
+function fizzBuzz(entrada){
+    if(typeof entrada !== 'number')
+        return 'Não é um número';
+    if((entrada % 3 === 0) && (entrada % 5 === 0)) {
+        return 'FizzBuzz'
+    }
+    if(entrada % 3 === 0){
+        return 'Fizz'
+    }
+    if(entrada % 5 === 0)
+    return 'Buzz'
 
 
-
-
-
+    return entrada;
+};
