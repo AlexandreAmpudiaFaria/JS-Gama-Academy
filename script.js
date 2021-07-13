23979af3544f4f5f77880280683486cf3ed7790d
Alexandre Faria <alexandre_faria123@icloud.com>
2021-07-12 23:55:07 -0300

    Lista de Tarefas

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-function convertToFahrenheit(value) {
-    return value * 1.8 + 32;
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+let lists = []
+    
+
+newListForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const ListName = newListInput.value
+    if (ListName === null || ListName === '') return
+    const list = createList(ListName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
+
+function createList(name){
+    return {id: Date.now().toString(), name: name}
 }
 
-let result = convertToFahrenheit(40);
-console.log(`O valor em fahrenheit é: ${result}`)
+function render() {
+    clearElement(listContainer)
+    lists.forEach(function(list){
+        const item = document.createElement('li')
+        item.classList.add('item')
+        item.innerText = list.name
+        listContainer.appendChild(item)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
